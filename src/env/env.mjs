5ecab8803babeb1b994e76cd3a57ebedabe462ec
Brendan Elton <brendan.elton@gmail.com>
2024-05-01 02:10:38 +1000

    Update: Envs MJS

--- src/env/env.mjs
+++ src/env/env.mjs
@@ -4,10 +4,16 @@ import { createEnv } from '@t3-oss/env-nextjs'
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
+    NEXT_PUBLIC_EMAIL_USERNAME: z.string(),
+    NEXT_PUBLIC_EMAIL_APP_PASSWORD: z.string(),
+    NEXT_PUBLIC_PERSONAL_EMAIL: z.string(),
   },
   client: {},
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_EMAIL_USERNAME: process.env.NEXT_PUBLIC_EMAIL_USERNAME,
+    NEXT_PUBLIC_EMAIL_APP_PASSWORD: process.env.NEXT_PUBLIC_EMAIL_APP_PASSWORD,
+    NEXT_PUBLIC_PERSONAL_EMAIL: process.env.NEXT_PUBLIC_PERSONAL_EMAIL,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 })
